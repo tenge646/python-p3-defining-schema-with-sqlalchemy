20c83572db7d1404d37526cf1eeb506dc1232ef9
tenge646 <144627741+tenge646@users.noreply.github.com>
2023-12-08 19:18:47 +0300

    completed assignment

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,6 +1,6 @@
 # lib/sqlalchemy_sandbox.py
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
